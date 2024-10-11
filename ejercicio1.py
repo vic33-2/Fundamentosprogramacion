c75d42ab1503dc341d5dd80d525d123461ca17a0
unknown <victoralangonzalez8@gmail.com>
2024-10-11 12:40:04 -0600

    01 ejrecicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+'''
+Ejercicio 1:
+    Escribir un programa que pregunte a la usuario su  nomnre
+    e imroima un saludo 
+Entradas:
+     nombre: string
+Salida: 
+    saludo string    
+'''
+nombre = input('Escribe tu nombre: ')
+edad = input('escribe tu edad:  ')
+saludo = "Hola " + nombre + "de " + edad + "anos " " ! "
+print(saludo)
+
