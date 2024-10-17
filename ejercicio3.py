83e4ab168210713faa90ec37dd53c8f596aa7e98
unknown <victoralangonzalez8@gmail.com>
2024-10-17 10:00:36 -0600

    04 Ejercicio 4

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,7 +6,7 @@ Entradas:
     cateto1: int
     cateto2: int
 Salidas:
-    hipotenuza
+    hipotenusa
 Analisis
     Se resuelve con el teorema de pitagoras
 '''
@@ -14,7 +14,7 @@ import math
 cateto1 = input("Escribe el cateto 1: ")
 cateto1 = int(cateto1)
 cateto2 = int(input('escribe el catto 2'))
-hipotenuza = cateto1 * cateto1 + cateto2 * cateto2
-hipotenuza = hipotenuza ** (1/2)
-hipotenuza = math.sqrt (hipotenuza)
-print(' la hipotenusa es: ', hipotenuza)
+hipotenusa = cateto1 * cateto1 + cateto2 * cateto2
+hipotenusa = hipotenusa ** (1/2)
+hipotenusa = math.sqrt (hipotenusa)
+print(' la hipotenusa es: ', hipotenusa)
---
+++ ejercicio4.py
@@ -0,0 +1,25 @@
+'''
+Ejercicio 4:
+    Programa que muestra la suma, resta, division y 
+    multliplicacion de ambos
+Entradas:
+    num1: int
+    num2: int
+Salidas:
+suma
+resta
+division
+multiplicacion 
+'''
+num1 = input("ingresa el primer numero: ")
+num1 = int(num1)
+num2 = input("ingresa el segundo numero: ")
+num2 = int(num2)
+suma = num1 + num2
+resta = num1 + num2
+multiplicacion = num1 * num2
+division = num1 / num2
+print("Lasuma es:", suma)
+print("La resta es:", resta)
+print("La multiplicacion es:", multiplicacion)
+print("La division es:", division)
