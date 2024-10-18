18dc3be5fe236d440b9d089458951c3216990349
unknown <victoralangonzalez8@gmail.com>
2024-10-18 12:41:33 -0600

    06 Ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,21 @@
+'''
+  Calcular la media de tres numeros pedidos
+  por teclado
+Entradas:
+    num1: int
+    num2: int
+    num3: int
+Salidas:
+suma    
+'''
+
+num1 = input ("ingresa el segundo numero: ")
+num1 = int(num1)
+num2 = input("ingresa el segundo numero: ")
+num2 = int(num2)
+num3 = input("ingresa el tercer numero: ")
+num3 = int(num3)
+suma = num1 + num2 + num3
+promedio = suma + num3
+print("La suma es:", suma) 
+print("promedio es:", promedio)
